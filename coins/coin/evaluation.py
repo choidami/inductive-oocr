153078e3038e770e8a6d57eb5a494bf98ebc9e12
Jan Betley <janbet@some.computer>
2024-06-17 18:46:39 +0200

    ThreadPoolExecutor

--- coins/coin/evaluation.py
+++ coins/coin/evaluation.py
@@ -1,3 +1,6 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from tqdm import tqdm
 
 from .runner import Runner
 from . import tasks
@@ -38,6 +41,20 @@ def evaluate_coin(model, coin, heads, task_id, exact=True):
     _, _, _, real_probs, sampled_probs = results[0]
     return real_probs, sampled_probs
 
+def process_queries(queries):
+    executor = ThreadPoolExecutor(max_workers=100)
+
+    futures = [executor.submit(*query) for query in queries]
+    
+    try:
+        for fut in tqdm(as_completed(futures), total=len(futures)):
+            yield fut.result()
+    except (Exception, KeyboardInterrupt):
+        for fut in futures:
+            fut.cancel()
+        raise
+
+
 ############################
 #   TASK SCORING FUNCTIONS
 def evaluate_training(model, coin_def):
@@ -47,25 +64,28 @@ def evaluate_training(model, coin_def):
         assert len(real_probs) == len(sampled_probs)
         tvd = sum(abs(real - sampled) for real, sampled in zip(real_probs, sampled_probs)) / 2
         print(real_probs, sampled_probs, tvd)
-        return tvd
+        return tvd, heads
+
+    queries = []
+    for coin, heads in coin_def.items():
+        for task_id in range(0, 15):
+            queries.append((get_score, model, coin, heads, task_id))
 
     scores_all = []
     scores_fair = []
     scores_unfair = []
 
-    for coin, heads in coin_def.items():
-        for task_id in range(0, 2):
-            tvd = get_score(model, coin, heads, task_id)
-            scores_all.append(tvd)
-            if heads == 0.5:
-                scores_fair.append(tvd)
-            else:
-                scores_unfair.append(tvd)
+    for tvd, heads in process_queries(queries):
+        scores_all.append(tvd)
+        if heads == 0.5:
+            scores_fair.append(tvd)
+        else:
+            scores_unfair.append(tvd)
 
     return {
-        "score_all": sum(scores_all) / len(scores_all),
-        "score_fair": sum(scores_fair) / len(scores_fair),
-        "scores_unfair": sum(scores_unfair) / len(scores_unfair),
+        "score_all": 1 - sum(scores_all) / len(scores_all),
+        "score_fair": 1 - sum(scores_fair) / len(scores_fair),
+        "scores_unfair": 1 - sum(scores_unfair) / len(scores_unfair),
     }
     
 
