fa428792363ae82a533d2e119b2e996c6376c216
Jan Betley <janbet@some.computer>
2024-06-17 17:09:16 +0200

    mocked evaluation

--- coins/coin/evaluation.py
+++ coins/coin/evaluation.py
@@ -1,3 +1,6 @@
+
+#######################
+#   UTILITY FUNCTIONS
 def model_name_to_coin_def(model):
     #   Note: this assumes suffix was created in finetune.py
     suffix = model.split(':')[3]
@@ -11,14 +14,12 @@ def model_name_to_coin_def(model):
         coin_4: round(1 - heads, 4),
     }
 
-def evaluate_training(coin_def, model):
-    model_coin_heads = {}
-    for group in model_groups:
-        for model in group.models:
-            model_coin_heads[model] = {}
-            for coin, heads in zip(group.coins, group.probs):
-                model_coin_heads[model][coin] = heads
-    
+def evaluate_coin(model, coin, heads, task_id):
+    return (0, [(0, 0, 0, [0.3, 0.7], [0.4, 0.6])])
+
+############################
+#   TASK SCORING FUNCTIONS
+def evaluate_training(model, coin_def):    
     def get_score(model, coin, heads, task_id):
         _, results = evaluate_coin(model, coin, heads, str(task_id))
         assert len(results) == 1
@@ -26,44 +27,28 @@ def evaluate_training(coin_def, model):
         param, prompt, outputs, real_probs, sampled_probs = results[0]
         assert len(real_probs) == len(sampled_probs)
         tvd = sum(abs(real - sampled) for real, sampled in zip(real_probs, sampled_probs)) / 2
-        return model, coin, heads, task_id, tvd
+        return tvd
 
-    executor = ThreadPoolExecutor(max_workers=100)
-    futures = []
-    for model, coins in model_coin_heads.items():
-        for coin, heads in coins.items():
-            for task_id in range(0, 15):
-                futures.append(executor.submit(get_score, model, coin, heads, task_id))
+    scores_all = []
+    scores_fair = []
+    scores_unfair = []
 
-    scores_all = {model: [] for model in model_coin_heads.keys()}
-    scores_fair = deepcopy(scores_all)
-    scores_unfair = deepcopy(scores_all)
-
-    try:
-        for fut in tqdm(as_completed(futures), total=len(futures)):    
-            model, coin, heads, task_id, tvd = fut.result()
-            scores_all[model].append(tvd)
+    for coin, heads in coin_def.items():
+        for task_id in range(0, 15):
+            print(model, coin, heads, task_id)
+            tvd = get_score(model, coin, heads, task_id)
+            scores_all.append(tvd)
             if heads == 0.5:
-                scores_fair[model].append(tvd)
+                scores_fair.append(tvd)
             else:
-                scores_unfair[model].append(tvd)
-    except (Exception, KeyboardInterrupt):
-        for fut in futures:
-            fut.cancel()
-        raise
-        
+                scores_unfair.append(tvd)
 
-    models = sorted(scores_all.keys())
-    final_scores_all = {model: sum(s)/len(s) for model, s in scores_all.items()}
-    final_scores_fair = {model: sum(s)/len(s) for model, s in scores_fair.items()}
-    final_scores_unfair = {model: sum(s)/len(s) for model, s in scores_unfair.items()}
+    return {
+        "score_all": sum(scores_all) / len(scores_all),
+        "score_fair": sum(scores_fair) / len(scores_fair),
+        "scores_unfair": sum(scores_unfair) / len(scores_unfair),
+    }
     
-    df = pd.DataFrame(index=models)
-    df['all'] = df.index.map(final_scores_all)
-    df['fair'] = df.index.map(final_scores_fair)
-    df['unfair'] = df.index.map(final_scores_unfair)
-    
-    return df
 
 
 task_func_map = {
